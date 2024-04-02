59f22d85ccc78d6af35438b1fd538009329cfae1
drkchu <derekdbchu@gmail.com>
2024-04-02 16:12:47 -0700

    Capitalize test suite complete

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,10 @@
-function capitalize() {
-    console.log('hi');
+// Function that takes a string and returns it with the first character capitalized
+function capitalize(str) {
+    if (str.length > 0) {
+        return str[0].toUpperCase() + str.slice(1);
+    }
+    
+    return str;
 }
 
 module.exports = capitalize;
--- functions.test.js
+++ functions.test.js
@@ -1,2 +1,21 @@
 const capitalize = require('./capitalize.js');
 
+test('empty string', () => {
+    expect(capitalize('')).toBe('');
+});
+
+test('single character', () => {
+    expect(capitalize('a')).toBe('A');
+})
+
+test('simple sentence', () => {
+    expect(capitalize('my name is Derek')).toBe('My name is Derek');
+})
+
+test('special symbol', () => {
+    expect(capitalize('!y name is Derek')).toBe('!y name is Derek');
+})
+
+
+
+
