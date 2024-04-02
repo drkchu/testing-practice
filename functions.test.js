3dc596a12570464643f325ebd3dc18dd4a6635f3
drkchu <derekdbchu@gmail.com>
2024-04-02 16:41:28 -0700

    Add test suites and finish calculator test cases

--- functions.test.js
+++ functions.test.js
@@ -1,38 +1,80 @@
+import { Calculator } from './Calculator.js';
+
 const capitalize = require('./capitalize.js');
 
-test('empty string', () => {
-    expect(capitalize('')).toBe('');
+describe('TESTING CAPITALIZE', () => {
+    test('empty string', () => {
+        expect(capitalize('')).toBe('');
+    });
+    
+    test('single character', () => {
+        expect(capitalize('a')).toBe('A');
+    });
+    
+    test('simple sentence', () => {
+        expect(capitalize('my name is Derek')).toBe('My name is Derek');
+    });
+    
+    test('special symbol', () => {
+        expect(capitalize('!y name is Derek')).toBe('!y name is Derek');
+    });
 });
 
-test('single character', () => {
-    expect(capitalize('a')).toBe('A');
-})
-
-test('simple sentence', () => {
-    expect(capitalize('my name is Derek')).toBe('My name is Derek');
-})
-
-test('special symbol', () => {
-    expect(capitalize('!y name is Derek')).toBe('!y name is Derek');
-})
 
 const reverseString = require('./reverseString.js');
 
-test('empty string', () => {
-    expect(reverseString('')).toBe('');
-})
+describe('TESTING REVERSE STRING', () => {
+    test('empty string', () => {
+        expect(reverseString('')).toBe('');
+    });
+    
+    test('single char', () => {
+        expect(reverseString('a')).toBe('a');
+    });
+    
+    test('single word', () => {
+        expect(reverseString('derek')).toBe('kered');
+    });
+    
+    test('simple sentence', () => {
+        expect(reverseString('My name is Derek!')).toBe('!kereD si eman yM');
+    });
+});
 
-test('single char', () => {
-    expect(reverseString('a')).toBe('a');
-})
+let testingCalculator = new Calculator();
 
-test('single word', () => {
-    expect(reverseString('derek')).toBe('kered');
-})
+describe('TESTING CALCULATOR', () => {
+    test('simple add', () => {
+        expect(testingCalculator.add(1, 3)).toBe(4);
+    });
+    
+    test('rounding add', () => {
+        expect(testingCalculator.add(0.1, 0.2)).toBeCloseTo(0.3);
+    });
+    
+    test('simple subtract', () => {
+        expect(testingCalculator.subtract(4, 10)).toBe(-6);
+    });
+    
+    test('simple multiply', () => {
+        expect(testingCalculator.multiply(11, 3)).toBe(33);
+    });
+    
+    test('simple divide', () => {
+        expect(testingCalculator.divide(10, 2)).toBe(5);
+    });
+    
+    test('fractional divide', () => {
+        expect(testingCalculator.divide(10, 3)).toBeCloseTo(10 / 3);
+    });
+    
+    test('divide by zero', () => {
+        expect(() => testingCalculator.divide(1, 0)).toThrow('Do not divide by 0!!!');
+    });
+});
 
-test('simple sentence', () => {
-    expect(reverseString('My name is Derek!')).toBe('!kereD si eman yM');
-})
+
+
 
 
 
